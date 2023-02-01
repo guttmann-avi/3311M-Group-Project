6a675f2de8bd3f4dcbae484e22831b8e7fade2ed
guttmann-avi <77285621+guttmann-avi@users.noreply.github.com>
2023-02-01 08:46:14 -0500

    Basic GUI files
    to run the main.java file

---
+++ BudgetingSystem/GUI.java
@@ -0,0 +1,19 @@
+package BudgetingSystem;
+
+public class GUI extends JFrame implements ItemListener, ActionListener {
+    public GUI() {
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ BudgetingSystem/Main.java
@@ -0,0 +1,9 @@
+package BudgetingSystem;
+public class Main {
+
+    public static void main(String[] args) {
+        GUI window = new GUI();
+        window.setVisible(true);
+    }
+
+}
