87238a485cc8c85c688b18f7c0faed9f140740f4
KavyanNasseri <77510794+KavyanNasseri@users.noreply.github.com>
2023-02-02 21:28:05 -0500

    Updated Package with Purchases Class

---
+++ Budgeting System/Purchases.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Purchases {
+	private static final int Id = 0; 
+	private double amount; 				// Make sure to only show two decimal places
+	private String category;			// Not for Iteration 1 
+	private Date date;
+	Purchases(){
+		amount = 0;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+}
