228b0059d057b1e11b79715fc9c562279589426c
KavyanNasseri <77510794+KavyanNasseri@users.noreply.github.com>
2023-02-06 21:30:57 -0500

    UserInformation Constructors

--- PrintingInformation/UserInformation.java
+++ PrintingInformation/UserInformation.java
@@ -1,15 +1,16 @@
 package PrintingInformation;
 
+import java.util.Date;
+
 public class UserInformation  implements TotalInformation{
-
-
-
+    private String info;
+    public UserInformation(String userName, double totalIncome, Date creationDate){
+        //logic 
+        printinfo();
+    }
     public String printinfo(){
 
-
-    
-
-        return null;
+        return this.info;
         }
     
 }
