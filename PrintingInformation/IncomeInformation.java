4786bb59848cd7590248b81a8264caa271ee3e7e
Abdullah-Djama <67915599+Abdullah-Djama@users.noreply.github.com>
2023-02-06 21:47:44 -0500

    IncomeInformation addition 1

--- PrintingInformation/IncomeInformation.java
+++ PrintingInformation/IncomeInformation.java
@@ -1,10 +1,22 @@
 package PrintingInformation;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import BudgetingSystem.Income;
+import BudgetingSystem.Purchases;
+
 public class IncomeInformation  implements TotalInformation{
     
+   Income income= new Income(0, null, null);// aggregation is used
+
+    public IncomeInformation(Income incomes)
+    {
+        printinfo();
+    }
     public IncomeInformation()
     {
-        
+        this(null);
     }
 
     public String printinfo(){
