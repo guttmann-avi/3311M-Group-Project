550829a14f9eafb2c7e5e3c6e22887a4a798d89d
Lacasse-Curtis-216779001 <curtlac@my.yorku.ca>
2023-02-07 19:38:34 -0500

    Added income functionality to household
    here I added the ability for a household to have an income based on the income of it's users. If there are no users, the income defaults to nothing

--- userImplements/Household.java
+++ userImplements/Household.java
@@ -3,71 +3,94 @@ package userImplements;
 import java.util.ArrayList;
 import java.util.List;
 import BudgetingSystem.Purchases;
+import BudgetingSystem.Income;
 
 public class Household implements userManager {
 
-    private List<User> users;
-    private String name;
-    private static int nexthouseID = 0;
-    private int houseID;
-    ArrayList<Purchases> purchases;
+	private List<User> users;
+	private String name;
+	private static int nexthouseID = 0;
+	private int houseID;
+	private Income income;
+	ArrayList<Purchases> purchases;
 
-    public Household() {
-        this.users = new ArrayList<>();
-        this.houseID = nexthouseID++;
-        
-    }
+	public Household() {
+		this.users = new ArrayList<>();
+		this.houseID = nexthouseID++;
+		this.income = new Income(0,null,null);
 
-    public String getName() {
+	}
+	// Next iteration we should make an interface for get/set name/ID as to not have
+	// duplicate code
+
+	public String getName() {
 		return name;
 	}
-    public void setName(String name) {
-    	this.name = name;
-    }
-    public void setname(String name) {
-    	this.name = name;
-    }
-    
-    public int getHouseID() {
-    	return this.houseID;
-    }
-    
-    public void addPurchases(Purchases purchases) {
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHouseID() {
+		return this.houseID;
+	}
+
+	public void addPurchases(Purchases purchases) {
 		this.purchases.add(purchases);
 	}
-    
-    @Override
-    public void addUser(User user) {
-        this.users.add(user);
-    }
 
-    @Override
-    public void removeUser(User user) {
-        this.users.remove(user);
-    }
-    
+	@Override
+	public void addUser(User user) {
+		this.users.add(user);
+		// adds income of each user in household as they are added, defaulting the date
+		// and source to the same as the  first user
+		double incomeHelper = 0;
+		for (int i = 0; i < users.size(); i++) {
+			incomeHelper += users.get(i).getIncome().getAmount();
+		}
+		this.income = new Income(incomeHelper, users.get(0).getIncome().getSource(),
+				users.get(0).getIncome().getDate());
+	}
 
-    public List<User> getUsers() {
-        return users;
-    }
+	@Override
+	public void removeUser(User user) {
+		this.users.remove(user);
+		// this fixes the income of the household if a user is removed,
 
-    public User findUser(String name) {
-        for (User user : users) {
-            if (user.getName().equalsIgnoreCase(name)) {
-                return user;
-            }
-        }
-        return null;
-    }
+		if (users.isEmpty()) {
+			this.income = new Income(0, null, null);
+		} else {
+			double incomeHelper = 0;
+			for (int i = 0; i < users.size(); i++) {
+				incomeHelper += users.get(i).getIncome().getAmount();
+			}
+			// this may cause problems if there is no user at users(0)
+			this.income = new Income(incomeHelper, users.get(0).getIncome().getSource(),
+					users.get(0).getIncome().getDate());
+		}
+	}
 
-    public void replaceUser(User replacedUser) {
-        for (User user : users) {
-            if (user.getName().equalsIgnoreCase(replacedUser.getName())) {
-                int index = users.indexOf(user);
-                users.remove(user);
-                users.add(index, replacedUser);
-                break;
-            }
-        }
-    }
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public User findUser(String name) {
+		for (User user : users) {
+			if (user.getName().equalsIgnoreCase(name)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public void replaceUser(User replacedUser) {
+		for (User user : users) {
+			if (user.getName().equalsIgnoreCase(replacedUser.getName())) {
+				int index = users.indexOf(user);
+				users.remove(user);
+				users.add(index, replacedUser);
+				break;
+			}
+		}
+	}
 }
