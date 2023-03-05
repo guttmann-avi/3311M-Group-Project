def978def17304360b023ee766b0a10c4bbcffea
guttmann-avi <77285621+guttmann-avi@users.noreply.github.com>
2023-03-04 21:10:02 -0500

    UserImplements folder renamed

---
+++ src/UserImplements/Household.java
@@ -0,0 +1,112 @@
+package src.UserImplements;
+
+import java.util.ArrayList;
+import java.util.List;
+import src.BudgetingSystem.Purchases;
+import src.BudgetingSystem.Income;
+
+public class Household implements UserManager {
+
+	private List<User> users;
+	private String name;
+	private static int nexthouseID = 0;
+	private int houseID;
+	private Income income;
+	ArrayList<Purchases> purchases;
+
+	public Household() {
+		this.users = new ArrayList<>();
+		this.houseID = nexthouseID++;
+		this.income = new Income(0,null,null);
+		this.purchases = new ArrayList<Purchases>();
+
+	}
+
+	// Next iteration we should make an interface for get/set name/ID as to not have
+	// duplicate code
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHouseID() {
+		return this.houseID;
+	}
+
+	public Income getIncome() {
+		return income;
+	}
+
+	public void setIncome(Income income) {
+		this.income = income;
+	}
+
+	public void addPurchases(Purchases purchases) {
+		this.purchases.add(purchases);
+	}
+
+	public void addPurchases(ArrayList<Purchases> purchases) {
+		this.purchases = purchases;
+	}
+
+	public ArrayList<Purchases> getPurchases() {
+		return this.purchases;
+	}
+
+	@Override
+	public void addUser(User user) {
+		this.users.add(user);
+		// adds income of each user in household as they are added, defaulting the date
+		// and source to the same as the  first user
+		double incomeHelper = 0;
+		for (int i = 0; i < users.size(); i++) {
+			incomeHelper += users.get(i).getIncome().getAmount();
+		}
+		this.income = new Income(incomeHelper, users.get(0).getIncome().getSource(),
+				users.get(0).getIncome().getDate());
+	}
+
+	@Override
+	public void removeUser(User user) {
+		this.users.remove(user);
+		// this fixes the income of the household if a user is removed,
+		if (users.isEmpty()) {
+			this.income = new Income(0, null, null);
+		} else {
+			double incomeHelper = 0;
+			for (int i = 0; i < users.size(); i++) {
+				incomeHelper += users.get(i).getIncome().getAmount();
+			}
+			// this may cause problems if there is no user at users(0)
+			this.income = new Income(incomeHelper, users.get(0).getIncome().getSource(),
+					users.get(0).getIncome().getDate());
+		}
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public User findUser(String name) {
+		for (User user : users) {
+			if (user.getName().equalsIgnoreCase(name)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public void replaceUser(User replacedUser) {
+		for (User user : users) {
+			if (user.getName().equalsIgnoreCase(replacedUser.getName())) {
+				int index = users.indexOf(user);
+				users.remove(user);
+				users.add(index, replacedUser);
+				break;
+			}
+		}
+	}
+}
---
+++ src/UserImplements/UserManager.java
@@ -0,0 +1,6 @@
+package src.UserImplements;
+
+public interface UserManager {
+	public void addUser(User user); // uses assocation
+	public void removeUser(User user);
+}
