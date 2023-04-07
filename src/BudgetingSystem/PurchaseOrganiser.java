e3756471872838513c5371f8885b776957af14fe
Lacasse-Curtis-216779001 <curtlac@my.yorku.ca>
2023-04-06 22:56:07 -0400

    FIXED

--- src/BudgetingSystem/PurchaseOrganiser.java
+++ src/BudgetingSystem/PurchaseOrganiser.java
@@ -8,29 +8,35 @@ import java.util.Map;
 public class PurchaseOrganiser {
 	private static final Map<String, List<String>> CategoryList = new HashMap<>();
 	static {
-		CategoryList.put("Groceries", Arrays.asList("food", "drinks", "toiletries", "grocery", "pharmacy", "shopping"));
-		CategoryList.put("Clothing", Arrays.asList("clothing", "shoes", "clothes", "boots"));
-		CategoryList.put("Entertainment", Arrays.asList("movie", "music", "game", "book", "sports"));
-		CategoryList.put("Appliance", Arrays.asList("phone", "fridge", "machine", "dryer", "computer"));
-		CategoryList.put("Transportation", Arrays.asList("car", "bus", "plane", "cab", "train", "subway", "gas"));
+		CategoryList.put("Groceries", Arrays.asList("food", "drinks", "toiletries", "grocery", "pharmacy", "shopping","groceries"));
+		CategoryList.put("Clothing", Arrays.asList("clothing", "shoes", "clothes", "boots","clothing"));
+		CategoryList.put("Entertainment", Arrays.asList("movie", "music", "game", "book", "sports","entertainment"));
+		CategoryList.put("Appliance", Arrays.asList("phone", "fridge", "machine", "dryer", "computer","appliance"));
+		CategoryList.put("Transportation", Arrays.asList("car", "bus", "plane", "cab", "train", "subway", "gas","transportation"));
 		CategoryList.put("Other", null);
 	}
 
 	public static void categorizePurchases(Purchases purchase) {
 		String keywordHelper = purchase.getCategory();
-		if (purchase.getCategory() != null) {
+		purchase.setCategory(keywordHelper);
+		breakpoint: if (purchase.getCategory() != null) {
 			for (Map.Entry<String, List<String>> category : CategoryList.entrySet()) {
 				if (category.getValue() != null) {
 					if (category.getValue().contains(keywordHelper.toLowerCase())) {
 						purchase.setCategory(category.getKey());
+						break breakpoint;
+					}
+					else {
+						purchase.setCategory("Other");
 					}
 				}
 
 			}
 		}
-			else if (purchase.getCategory() != null && purchase.getCategory() == keywordHelper) {
-				purchase.setCategory("Other");
-			} else {
+//			else if (purchase.getCategory() != null && purchase.getCategory() == keywordHelper) {
+//				purchase.setCategory("Other");
+	//}
+	else {
 				purchase.setCategory("Other");
 			}
 		}
